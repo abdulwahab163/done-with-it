fe7b0ce8e90c3796d76ab2d5b2f53c9d4c77f097
Abdul Wahab <abdul_wahab@elixir.com>
2021-02-08 19:49:02 +0500

    native features added

--- App.js
+++ App.js
@@ -1,34 +1,7 @@
-import React, { useEffect, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
+import React from "react";
 
-import { Image } from "react-native";
-import { Button } from "./app/components/Button";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
-
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) alert("you need to enable permissions");
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error Reading an image");
-    }
-  };
-
-  return (
-    <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
